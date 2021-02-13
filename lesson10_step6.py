f19b46f38a8f00f1a1f822235bbc8abc76c3a667
Vladislav Prorok <vprorok@ivi.ru>
2021-02-14 01:43:28 +0300

    old python tasks

---
+++ lesson10_step6.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/cats.html")
+    browser.find_element_by_id("button")
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
